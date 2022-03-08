434fffbe79ccde997eca0b0147466311cd9e1f4b
Heitor <hsbicalho@gmail.com>
2022-03-08 15:15:46 -0300

    requisito 4 implementado

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const circle = require('../src/circle');
-
+const assert = require('assert');
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
   Se não for especificado um raio, a função retorna undefined.
@@ -23,16 +23,31 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+describe('4 -Casos de teste para a função circle', () => {
+  it('Testa se circle retorna undefined, caso o parâmetro passado não seja um número.', () => {
+    assert.strictEqual(circle('st'), undefined);
   });
+  it('Testa se circle retorna um objeto.', () => {
+    assert.strictEqual(typeof circle(2), 'object');
+  })
+  it('Testa se o objeto retornado tem 3 propriedades.', () => {
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
+  })
+  it('Testa se a função, quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    assert.strictEqual(circle(), undefined);
+  })
+  it('Testa se função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.', () => {
+    assert.strictEqual((circle(2)).circumference, 12.56);
+  })
+  it('Testa se função retorna, dentro de um objeto, a área correta para um círculo de raio 3.', () => {
+    assert.strictEqual((circle(3)).area, 28.259999999999998);
+  })
+  it('Testa se a função retorna, num objeto, os dados corretos de um círculo de raio 3.', () => {
+    const objCircle = circle(3);
+    assert.deepStrictEqual((objCircle), {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84
+    });  
+  })
 });
