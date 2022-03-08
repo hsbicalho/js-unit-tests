e123a395bd60dad52f51bab71d4dab16518cc0f9
Heitor <hsbicalho@gmail.com>
2022-03-08 16:22:44 -0300

    requisitos 6 e 7 implementados

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const numbers = require('../src/numbers');
-const assert = require('assert');
+//const assert = require('assert');
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
 
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,5 +1,6 @@
 const productDetails = require('../src/productDetails');
-
+const assert = require('assert');
+const products = productDetails('Alcool gel', 'Máscara');
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
 
@@ -27,14 +28,23 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Verifica se productDetails é uma função', () => {
+    expect(typeof(productDetails)).toBe('function');
   });
+  it('Teste se o retorno da função é um array.', () => {
+  assert.deepStrictEqual(Array.isArray(productDetails()), true);
+  }) 
+  it('Teste se o array retornado pela função contém dois itens dentro.', () => {
+  assert.deepStrictEqual(Object.keys(productDetails()).length, 2);
+  })
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    assert.deepStrictEqual(typeof productDetails(), 'object');
+  })
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    assert.notDeepStrictEqual(products[0], products[1]);
+  })
+  it('Teste se os dois productIds terminam com 123.', () => {
+    assert.strictEqual((/123$/.test(products[0].details.productId) &&
+    /123$/.test(products[1].details.productId)), true);
+  })
 });
